d9543ed577ed86f3c4f0acaa21baf9718b75a476
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2018-10-30 17:24:23 -0500

    Start Game beg

--- gameStanford/ViewController.swift
+++ gameStanford/ViewController.swift
@@ -8,18 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController
+{
+    var flipCount = 0
+    flipCount += 1
+    flipCard(withEmoji: "👻", on: sender)
+    
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "👻", on: sender)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCard(withEmoji:"🎃", on: sender)
     }
-
-
+    
+    }
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    }
 }
 
