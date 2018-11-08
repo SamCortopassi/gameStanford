063a5e34070d612dd9b2481c109f271aa9498469
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2018-11-08 11:31:10 -0600

    broken button

--- gameStanford/ViewController.swift
+++ gameStanford/ViewController.swift
@@ -43,9 +43,13 @@ class ViewController: UIViewController
     }
             var emojiChoices = ["🦇","😱","🙀","😈","🎃","👻","🍭","🍬","🍎"]
     
-    
+    var emoji = [Int:String]()
             
     func emoji (for card: Card) -> String {
-                return "?"
+        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+        }
+        return emoji[card.identifier] ?? "?"
     }
 }
