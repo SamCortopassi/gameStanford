03c125c23ca9869ccae73e4971a55f892d71454f
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2018-11-16 11:58:18 -0600

    up down

--- gameStanford/Concentration.swift
+++ gameStanford/Concentration.swift
@@ -12,12 +12,30 @@ class Concentration
 {
     var cards = [Card]()
     
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
     func chooseCard(at index: Int) {
-        if cards[index].isFaceUp {
-            cards[index].isFaceUp = false
-        } else {
-            cards[index].isFaceUp = true
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                //check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                    indexOfOneAndOnlyFaceUpCard = nil
+                
+            } else {
+                // either no cards or 2 cards are face up
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+            
         }
+      
     }
     
     init(numberOfPairsOfCards: Int){
