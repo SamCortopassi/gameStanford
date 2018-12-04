8c34cde4c41f77701e240337d274bb832409a65c
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2018-12-04 16:08:57 -0600

    extension closure

--- gameStanford/Concentration.swift
+++ gameStanford/Concentration.swift
@@ -16,17 +16,18 @@ class Concentration
     
     private var indexOfOneAndOnlyFaceUpCard: Int? {
         get {
-            var foundIndex: Int?
-            for index in cards.indices {
-                if cards[index].isFaceUp {
-                    if foundIndex == nil {
-                        foundIndex = index
-                    } else {
-                        return nil
-                    }
-                }
-            }
-            return foundIndex
+            return cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly
+//            var foundIndex: Int?
+//           for index in cards.indices {
+//                if cards[index].isFaceUp {
+//                    if foundIndex == nil {
+//                        foundIndex = index
+//                    } else {
+//                        return nil
+//                    }
+//                }
+//            }
+//            return foundIndex
         }
         set {
             for index in cards.indices {
@@ -58,6 +59,13 @@ class Concentration
             cards += [card, card]
         }
         // todo: shuffle the cards
-       
     }
 }
+
+extension Collection {
+    var oneAndOnly: Element? {
+        return count == 1 ? first : nil
+    }
+}
+
+
